3f4498b06b3c6dbc8727708082b5df95797e56bf
Moaz Mubaydin <mmubaydin@gmail.com>
2024-11-12 15:48:13 +0100

    install mongoose

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-
+const mongoose = require("mongoose")
+const DATABASE_URL = "mongodb://127.0.0.1:27017/cohort-tools-api";
+const Cohort = require("./models/Cohorts.model")
 const PORT = 5005;
+const Schema = mongoose.Schema;
 
-// STATIC DATA
+// STATIC DATA 
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 const cohorts = require("./cohorts.json"); // added this day1
@@ -18,6 +21,13 @@ const cors = require("cors"); // Import CORS (R-D1)
 // Option 1: Enable CORS with default options:
 // app.use(cors());
 // Option 2: Restrictive access, allowing requests from specific IP addresses and domains
+
+
+mongoose
+  .connect(DATABASE_URL)
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
+
 app.use(
   cors({
     origin: ["http://localhost:5005", "http://localhost:5173/"], // Add the URLs of allowed origins to this array
