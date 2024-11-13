cd5cd51094cc4c25209c43414c30d8b2295c7504
nathidaum <daumnathi@gmail.com>
2024-11-13 15:55:50 +0100

    Add some more rules to the Schemas

--- server/models/Students.model.js
+++ server/models/Students.model.js
@@ -16,11 +16,7 @@ const studentSchema = new Schema({
   });
   
   // CREATE MODEL
-  // The model() method defines a model (Cohort, Student) and creates a collection (cohorts, students) in MongoDB
-  // The collection name will default to the lowercased, plural form of the model name:
-  
   const Student = mongoose.model("Student", studentSchema);
   
   // EXPORT THE MODELS
-  // Export both models in a single object
-  module.exports = { Student };
+  module.exports = Student;
