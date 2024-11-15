d2445061b8ac1720a8119c96187d344c7d8c8495
Moaz Mubaydin <mmubaydin@gmail.com>
2024-11-15 16:43:43 +0100

    code cleaning (prettier)

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,18 +1,9 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-  },
+  email: { type: String, unique: true, required: true },
+  name: { type: String, required: true },
+  password: { type: String },
 });
 
 module.exports = model("User", userSchema);
